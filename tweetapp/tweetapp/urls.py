ee678cf44c97f0c0dead984e483df9762ee481e7
vivek <vivekmishrabts@gmail.com>
2024-12-11 09:05:51 +0000

    this is some changes

--- tweetapp/tweetapp/urls.py
+++ tweetapp/tweetapp/urls.py
@@ -22,7 +22,7 @@ from django.contrib.auth.urls import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tweet/',include('tweet.urls')),
+    path('',include('tweet.urls')),
    
 
 ]
